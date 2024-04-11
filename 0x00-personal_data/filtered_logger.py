da50ee0ee583df40df5b0e4abeacb8e760e9d592
Menna Hassan <h.menna85@gmail.com>
2024-04-12 00:45:17 +0200

    task 2

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -7,8 +7,9 @@ from typing import List
 import re
 import logging
 
+PII_FIELDS = ('name', 'email', 'phone', 'ssn', 'password')
 
-def filter_datum(fields: List[str], redaction: str,
+def filter_datum(fields: tuple, redaction: str,
                  message: str, separator: str) -> str:
     """filter_datum fun"""
     for field in fields:
@@ -16,6 +17,17 @@ def filter_datum(fields: List[str], redaction: str,
                          rf'\1={redaction}{separator}',
                          message, flags=re.DOTALL)
     return message
+
+def get_logger() -> logging.Logger:
+    """get_logger fun"""
+    logger = logging.getLogger('user_data')
+    logger.setLevel(logging.INFO)
+    logger.propagate = False
+    hdlr = logging.StreamHandler()
+    fmt = RedactingFormatter(fields=PII_FIELDS)
+    hdlr.setFormatter(fmt)
+    logger.addHandler(hdlr)
+    return logger
 
 
 class RedactingFormatter(logging.Formatter):
@@ -26,7 +38,7 @@ class RedactingFormatter(logging.Formatter):
     FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
     SEPARATOR = ";"
 
-    def __init__(self, fields: List[str]):
+    def __init__(self, fields: tuple):
         super(RedactingFormatter, self).__init__(self.FORMAT)
         self.fields = fields
 
