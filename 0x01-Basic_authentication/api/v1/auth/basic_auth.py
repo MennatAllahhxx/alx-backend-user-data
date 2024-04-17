1477f347f6a876c60ee6f67b120ca7299701c512
Menna Hassan <h.menna85@gmail.com>
2024-04-17 03:37:28 +0200

    task 7

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -6,3 +6,19 @@ from api.v1.auth.auth import Auth
 
 class BasicAuth(Auth):
     """BasicAuth class"""
+
+    def extract_base64_authorization_header(self,
+                                            authorization_header: str) -> str:
+        """AI is creating summary for extract_base64_authorization_header
+
+        Args:
+            authorization_header (str): auth token (basic + token)
+
+        Returns:
+            str: token
+        """
+        if authorization_header is None or\
+           not isinstance(authorization_header, str) or \
+           authorization_header[0:6] != "Basic ":
+            return None
+        return authorization_header[6:]
