3bc98f59499488701afd8851456239ded6e50aad
Menna Hassan <h.menna85@gmail.com>
2024-04-18 23:17:31 +0200

    task 0

---
+++ 0x02-Session_authentication/api/v1/auth/auth.py
@@ -0,0 +1,55 @@
+#!/usr/bin/env python3
+""" Module of Authorization
+"""
+import os
+from typing import List, TypeVar
+
+
+class Auth:
+    """Auth class"""
+
+    def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
+        """AI is creating summary for require_auth
+
+        Args:
+            path (str): path to be checked
+            excluded_paths (List[str]): paths that dont require auth
+
+        Returns:
+            bool: False if path is in excluded_paths
+        """
+        if path is None or\
+           excluded_paths is None or\
+           excluded_paths == []:
+            return True
+        path = os.path.join(path, '')
+        for pathh in excluded_paths:
+            if pathh.endswith('*') and\
+               path.startswith(pathh[:-1]) or\
+               pathh == path:
+                return False
+        return True
+
+    def authorization_header(self, request=None) -> str:
+        """AI is creating summary for authorization_header
+
+        Args:
+            request ([type], optional): request. Defaults to None.
+
+        Returns:
+            str: the value of the header request Authorization
+        """
+        if request is None:
+            return None
+        if "Authorization" in request.headers:
+            return request.headers["Authorization"]
+        else:
+            return None
+
+    def current_user(self, request=None) -> TypeVar('User'):
+        """AI is creating summary for current_user
+
+        Returns:
+            [type]: [description]
+        """
+        return None
---
+++ 0x02-Session_authentication/api/v1/auth/basic_auth.py
@@ -0,0 +1,99 @@
+#!/usr/bin/env python3
+""" Module of Basic Authorization
+"""
+from api.v1.auth.auth import Auth
+from typing import TypeVar, List
+from models.user import User
+import base64
+
+
+class BasicAuth(Auth):
+    """BasicAuth class"""
+
+    def extract_base64_authorization_header(self,
+                                            authorization_header: str) -> str:
+        """AI is creating summary for extract_base64_authorization_header
+
+        Args:
+            authorization_header (str): auth token (basic + token)
+
+        Returns:
+            str: token
+        """
+        if authorization_header is None or\
+           not isinstance(authorization_header, str) or \
+           authorization_header[0:6] != "Basic ":
+            return None
+        return authorization_header[6:]
+
+    def decode_base64_authorization_header(self,
+                                           base64_authorization_header:
+                                           str) -> str:
+        """AI is creating summary for decode_base64_authorization_header
+
+        Args:
+            base64_authorization_header (str): encoded base64 token
+
+        Returns:
+            str: decoded value as UTF8 string
+        """
+        if base64_authorization_header is None or \
+           not isinstance(base64_authorization_header, str):
+            return None
+
+        try:
+            return base64.\
+                   b64decode(base64_authorization_header).\
+                   decode('utf-8')
+        except Exception:
+            return None
+
+    def extract_user_credentials(self,
+                                 decoded_base64_authorization_header:
+                                 str) -> (str, str):
+        """AI is creating summary for extract_user_credentials
+
+        Args:
+            str ([type]): [description]
+        """
+
+        if decoded_base64_authorization_header is None or\
+           not isinstance(decoded_base64_authorization_header, str)\
+           or ':' not in decoded_base64_authorization_header:
+            return (None, None)
+        info = decoded_base64_authorization_header.split(":")
+
+        return (info[0], ":".join(info[1:]))
+
+    def user_object_from_credentials(self,
+                                     user_email: str,
+                                     user_pwd: str) -> TypeVar('User'):
+        """AI is creating summary for user_object_from_credentials
+
+        Args:
+            self ([type]): [description]
+        """
+        if user_email is None or\
+           not isinstance(user_email, str) or\
+           user_pwd is None or\
+           not isinstance(user_pwd, str):
+            return None
+        try:
+            user = User.search({"email": user_email})
+        except Exception:
+            return None
+        if not user:
+            return None
+        if not user[0].is_valid_password(user_pwd):
+            return None
+        return user[0]
+
+    def current_user(self, request=None) -> TypeVar('User'):
+        """AI is creating summary for current_user
+        """
+        value_auth = self.authorization_header(request)
+        token = self.extract_base64_authorization_header(value_auth)
+        decode_token = self.decode_base64_authorization_header(token)
+        email, pwd = self.extract_user_credentials(decode_token)
+        user = self.user_object_from_credentials(email, pwd)
+        return user
