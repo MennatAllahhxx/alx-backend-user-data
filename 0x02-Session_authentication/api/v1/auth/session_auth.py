3683bab6cda6ea2cdf79fab830666bff14f54f11
Menna Hassan <h.menna85@gmail.com>
2024-04-18 23:33:20 +0200

    task 2

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -2,7 +2,25 @@
 """ Module of Session Authorization
 """
 from api.v1.auth.auth import Auth
+import uuid
 
 
 class SessionAuth(Auth):
     """SessionAuth class"""
+
+    user_id_by_session_id = {}
+
+    def create_session(self, user_id: str = None) -> str:
+        """AI is creating summary for create_session
+
+        Args:
+            user_id (str, optional): [description]. Defaults to None.
+
+        Returns:
+            str: [description]
+        """
+        if user_id is None or not isinstance(user_id, str):
+            return None
+        session_id = str(uuid.uuid4())
+        self.user_id_by_session_id[session_id] = user_id
+        return session_id
