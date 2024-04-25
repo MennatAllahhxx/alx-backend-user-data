ae07e9c24f17201afa6d4fa0cdb6ec5186fd8c81
Menna Hassan <h.menna85@gmail.com>
2024-04-26 02:37:03 +0300

    task 5

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -3,6 +3,9 @@
 Auth Module
 """
 
+from db import DB
+from user import User
+from sqlalchemy.orm.exc import NoResultFound
 import bcrypt
 
 
@@ -18,3 +21,28 @@ def _hash_password(password: str) -> bytes:
     byte = password.encode('utf-8')
     salt = bcrypt.gensalt()
     return bcrypt.hashpw(byte, salt)
+
+
+class Auth:
+    """Auth class to interact with the authentication database.
+    """
+
+    def __init__(self):
+        self._db = DB()
+
+    def register_user(self, email: str, password: str) -> User:
+        """AI is creating summary for register_user
+
+        Args:
+            email (str): [description]
+            password (str): [description]
+
+        Returns:
+            User: [description]
+        """
+        try:
+            user = self._db.find_user_by(email=email)
+            raise ValueError("User {} already exists".format(email))
+        except NoResultFound:
+            password = _hash_password(password)
+            return self._db.add_user(email, password)
