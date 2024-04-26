22c72fd6ae0bf1dd953e6d138e29d914fc81cf66
Menna Hassan <h.menna85@gmail.com>
2024-04-26 04:13:18 +0300

    task 14

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -1,50 +1,44 @@
 #!/usr/bin/env python3
-""" Module of App
 """
+flask app module to interact with the authentication database.
+"""
+
+
+import email
 from flask import Flask, jsonify, request, abort, make_response, redirect
 from auth import Auth
 
+
 app = Flask(__name__)
-auth = Auth()
+
+AUTH = Auth()
 
 
-@app.route('/', methods=['GET'], strict_slashes=False)
+@app.route("/")
 def index():
-    """ GET /
-    Return:
-      - jsonified message
-    """
+    """Index route"""
     return jsonify({"message": "Bienvenue"})
 
 
-@app.route('/users', methods=['POST'], strict_slashes=False)
+@app.route("/users", methods=["POST"])
 def register_user():
-    """ POST /users
-    Return:
-      - register a user
-    """
-    try:
-        email = request.form["email"]
-        password = request.form["password"]
-    except KeyError:
-        abort(400)
-    try:
-        user = auth.register_user(email, password)
-    except ValueError:
-        return jsonify({"message": "email already registered"}), 400
-    return jsonify({"email": email, "message": "user created"})
-
-
-@app.route('/sessions', methods=['POST'])
-def login():
-    """ POST /sessions
-    Return:
-      - log in
-    """
+    """Register a user in the database."""
     email = request.form["email"]
     password = request.form["password"]
-    if auth.valid_login(email, password):
-        session_id = auth.create_session(email)
+    try:
+        user = AUTH.register_user(email, password)
+        return jsonify({"email": user.email, "message": "user created"}), 200
+    except ValueError as err:
+        return jsonify({"message": "email already registered"}), 400
+
+
+@app.route("/sessions", methods=["POST"])
+def login():
+    """Login route"""
+    email = request.form["email"]
+    password = request.form["password"]
+    if AUTH.valid_login(email, password):
+        session_id = AUTH.create_session(email)
         if session_id:
             response = make_response(
                 jsonify({"email": email, "message": "logged in"}), 200
@@ -54,18 +48,51 @@ def login():
     abort(401)
 
 
-@app.route('/sessions', methods=['DELETE'])
+@app.route("/sessions", methods=["DELETE"])
 def logout():
-    """ DELETE /sessions
-    Return:
-      - log out
-    """
+    """Logout route"""
     session_id = request.cookies.get("session_id")
-    user = auth.get_user_from_session_id(session_id)
-    if user:
-        auth.destroy_session(user.id)
-        return redirect("/")
+    if session_id:
+        user = AUTH.get_user_from_session_id(session_id)
+        if user:
+            AUTH.destroy_session(session_id)
+            return redirect("/")
     abort(403)
+
+
+@app.route("/profile", methods=["GET"])
+def profile():
+    """Profile route"""
+    session_id = request.cookies.get("session_id")
+    if session_id:
+        user = AUTH.get_user_from_session_id(session_id)
+        if user:
+            return jsonify({"email": user.email}), 200
+    abort(403)
+
+
+@app.route("/reset_password", methods=["POST"])
+def get_reset_password_token():
+    """Get a reset password token"""
+    email = request.form["email"]
+    try:
+        token = AUTH.get_reset_password_token(email)
+        return jsonify({"email": email, "reset_token": token}), 200
+    except ValueError:
+        abort(403)
+
+
+@app.route("/reset_password", methods=["PUT"])
+def update_password():
+    """Update a user's password"""
+    token = request.form["reset_token"]
+    password = request.form["new_password"]
+    email = request.form["email"]
+    try:
+        AUTH.update_password(token, password)
+        return jsonify({"email": email, "message": "Password updated"}), 200
+    except ValueError:
+        abort(403)
 
 
 if __name__ == "__main__":
